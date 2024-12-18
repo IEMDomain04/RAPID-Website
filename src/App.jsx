056961625e9db009130bc91a4d9b3562d6d2cd81
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-18 16:16:55 +0800

    Fixed constant position

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import background from "./assets/background.svg";
 import Navbar from "./components/homepage/Navbar";
 import Home from "./components/homepage/Home";
 import Pricing from "./components/Pricing";
 import Learnmore from "./components/Learnmore";
 import Disclaimer from "./components/disclaimer";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+
+// Custom Scroll to Top Component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to the top of the page whenever the route changes
+  }, [pathname]);
+
+  return null; // No UI component, just side effects
+};
 
 const App = () => {
   return (
@@ -12,6 +25,7 @@ const App = () => {
       className="bg-cover sm:bg-center overflow-x-hidden"
       style={{ backgroundImage: `url(${background})` }}
     >
+      <ScrollToTop /> {/* Automatically scrolls to top on route changes */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,7 +33,7 @@ const App = () => {
         <Route path="/about" element={<Learnmore />} />
         <Route path="/disclaimer" element={<Disclaimer />} />
       </Routes>
-      <h1 className="text-center text-white mt-20 ">2024 @ ALLRIGHTS RESERVED BY EMMAN POGI</h1>
+      <h1 className="text-center text-white mt-20">2024 @ ALL RIGHTS RESERVED BY EMMAN POGI</h1>
     </div>
   );
 };
