d5aae951f1db51961c3ed4f0cc3d670f5dd756a3
IEMDomain04.dc <emmanman0011@gmail.com>
2025-03-27 21:58:26 +0800

    Added link to case study

--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -4,29 +4,31 @@ import RapidLogo from '../../assets/large-rapid-logo.svg';
 
 function Header() {
   const navigate = useNavigate();
-  
+
+
   return (
     <div className="flex flex-col-reverse md:flex-row justify-center items-center text-white px-8 py-10 mt-24 sm:mt-40">
       {/* Left Section */}
       <div data-aos="fade-right" data-aos-duration="3000" className="text-center md:text-left">
         <h1 className="text-5xl sm:text-6xl md:text-8xl font-bold mb-3">RAPID</h1>
         <h3 className="text-xl sm:text-2xl font-light">Real-Time. Collab. Connect. Develop. Innovate.</h3>
-        
+
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10 mt-12 justify-center md:justify-start">
           <button
             type="button"
-            onClick={() => navigate('/disclaimer')}
+            onClick={() => window.open('https://docs.google.com/presentation/d/1H0eUeTsffrGpgM0-4ekE0vUyqOivGZS9neg8yWOg6oc/edit#slide=id.ge4c37861fa_0_354')
+            }
             className="bg-blue-600 text-white px-10 py-2 rounded-md hover:bg-blue-700 transition"
           >
-            Try now
+            See website case study
           </button>
           <button
-      type="button"
-      onClick={() => navigate('/about')}
-      className="border-2 border-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-600 hover:text-white transition"
-    >
-      Learn more
-    </button>
+            type="button"
+            onClick={() => navigate('/about')}
+            className="border-2 border-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-600 hover:text-white transition"
+          >
+            Learn more
+          </button>
         </div>
       </div>
 
